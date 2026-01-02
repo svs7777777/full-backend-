528e498c9e5c0ac801b75c36d7beee963e94d450
vivek <sindhavvivekv@example.com>
2026-01-02 12:17:31 +0530

    changes done

--- src/utils/asyncHandeler.js
+++ src/utils/asyncHandeler.js
@@ -1,13 +1,24 @@
-// const asyncHandler =() => {}
+const asyncHandler =(requestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
 
 export  {asyncHandler}
 
 
 
-const asyncHandeler=(func)=>{
-    
-}
-const asyncHandeler=(fn)=>()=>{
+// const asyncHandeler=(fn)=>async(req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
 
+//     }
+//     catch(error){
+//         res.status(err.code  || 500).json({
+//             success:false,
+//             message:err.message
+//         })
 
-}
+//     }
+// }
